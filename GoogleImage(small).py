ec44b7155194908785790e4404059c5996e26292
DoctorEenot <47860375+DoctorEenot@users.noreply.github.com>
2019-06-06 17:05:17 +0300

    Add files via upload

---
+++ GoogleImage(small).py
@@ -0,0 +1,34 @@
+'''
+GoogleImage Api
+EnotProd
+'''
+import bs4
+import requests
+
+def getIm(req,pages):
+    page = 0
+    count = 0
+    while page != pages:
+        cont = requests.get('https://www.google.ru/search?q='+req+'&newwindow=1&biw=1406&bih=743&ie=UTF-8&tbm=isch&ei=CPu5XL7xDIeFmwWYuaG4Bw&start='+str(page)+'&sa=N')
+        dom = bs4.BeautifulSoup(cont.text, 'html.parser')
+        
+    
+        for link in dom.find_all('img'):
+            if count == 0:
+                count = 1
+                continue
+            #print(link.get('src'))
+            try:
+                r = requests.get(link.get('src')).content
+                
+            except:
+                continue
+            count = count + 1
+            file = open('Photo('+str(count)+').jpeg','wb')
+            file.write(r)
+            file.close()
+            
+        page = page + 1
+    
+getIm('ДНР',20)
+
